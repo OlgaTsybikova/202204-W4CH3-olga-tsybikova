c840ba2d20965ff5c231368c7cae9a84633bf5b7
Olga Tsybikova <tsy2701@gmail.com>
2022-04-27 22:59:50 +0200

    Added context to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import React, { useState } from "react";
 import KeyboardComponent from "./components/Keyboard/KeyboardComponent";
-
+export const TelefonoContext = React.createContext();
 function App() {
+  const [iscalling, setIsCalling] = useState(false);
   return (
     <>
-      <div className="container">
-        <span className="message">Calling...</span>
-        <main className="phone">
-          <KeyboardComponent />
-          <div className="actions">
-            <span className="number">667359961</span>
-            <a href>Call</a>
-            <a href>Hang</a>
-          </div>
-        </main>
-      </div>
+      <TelefonoContext.Provider value={iscalling}>
+        <div className="container">
+          <span className="message">Calling...</span>
+          <main className="phone">
+            <div className="keyboard-container">
+              <KeyboardComponent />
+            </div>
+            <div className="actions">
+              <span className="number">667359961</span>
+              <a href>Call</a>
+              <a href>Hang</a>
+            </div>
+          </main>
+        </div>
+      </TelefonoContext.Provider>
     </>
   );
 }
